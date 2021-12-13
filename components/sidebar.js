62605858f312bd915f24c6826b47b971fa4e29f7
ANURAG SINGH <adarshsaurav6613@gmail.com>
2021-12-13 21:43:40 +0530

    Made basic news app
    Now this app can show top news on homepage, user can search for specific news by going to search page.

---
+++ components/sidebar.js
@@ -0,0 +1,23 @@
+function sidebar(){
+    return `<div id="company">
+    <a href="Home.html">
+    <h1>TC</h1>
+    </a>
+    <h3>Join TechCruch+</h3>
+    <p>Login</p>
+  </div>
+  <a href="search.html">
+  <button >Search Q</button>
+  </a>
+  <button>TC Sessions:Space</button>
+  <button>Gift Guid</button>
+  <button>StartUps</button>
+  <button>Videos</button>
+  <button>Audio</button>
+  <button>Newsletters</button>
+  <button>TechCruch+</button
+  ><button>Advertise</button>
+  <button>Events</button>
+  <button>More</button>`
+}
+export default sidebar;
